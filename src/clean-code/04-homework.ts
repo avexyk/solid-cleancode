0bf26583c2fc9c527cf6ab04aa60419e9e3089c3
Francisco Javier Gil Bautista <avexyk@gmail.com>
2022-04-24 16:57:49 -0500

    add: principio dry

--- src/clean-code/04-homework.ts
+++ src/clean-code/04-homework.ts
@@ -4,57 +4,42 @@
   // Resolver sin la triple condicional dentro del if
   // includes? arrays?
   function isRedFruit( fruit: string ): boolean {
-      
-      if ( fruit === 'manzana' || fruit === 'cereza' || fruit === 'ciruela' ) {
-          return true;
-      } else {
-          return false;
-      }
+		const redFruits = ['manzana', 'cereza', 'ciruela'];
+		return redFruits.includes(fruit);
   }
 
   // Simplificar esta función
   // switch? Object literal? validar posibles colores
-  function getFruitsByColor( color: string ): string[] {
+	type FruitColor = 'red'|'yellow'|'purple';
 
-      if ( color === 'red' ) {
-          return ['manzana','fresa'];
-      } else if ( color === 'yellow') {
-          return ['piña','banana'];
-      } else if ( color === 'purple') {
-          return ['moras','uvas']
-      } else {
-          throw Error('the color must be: red, yellow, purple');
-      }
+  function getFruitsByColor( color: FruitColor ): string[] {
+		const fruitsByColor = {
+			red: ['manzana','fresa'],
+			yellow: ['piña','banana'],
+			purple: ['moras','uvas'],
+		}
+
+		if ( !Object.keys( fruitsByColor ).includes(color) ) {
+			throw Error('the color must be: red, yellow, purple');
+		}
+
+		return fruitsByColor[color];
   }
 
   // Simplificar esta función
   let isFirstStepWorking  = true;
   let isSecondStepWorking = true;
-  let isThirdStepWorking  = true;
+  let isThirdStepWorking  = false;
   let isFourthStepWorking = true;
 
   function workingSteps() {
-      if( isFirstStepWorking === true ) {
-          if( isSecondStepWorking === true ) {
-              if( isThirdStepWorking === true ) {
-                  if( isFourthStepWorking === true ) {
-                      return 'Working properly!';
-                  }
-                  else {
-                      return 'Fourth step broken.';
-                  }
-              }
-              else {
-                  return 'Third step broken.';
-              }
-          }
-          else {
-              return 'Second step broken.';
-          }
-      }
-      else {
-          return 'First step broken.';
-      }
+
+		if ( !isFirstStepWorking ) return 'First step broken.';  
+		if ( !isSecondStepWorking ) return 'Second step broken.';  
+		if ( !isThirdStepWorking ) return 'Third step broken.';  
+		if ( !isFourthStepWorking ) return 'Fourth step broken.';
+		
+		return 'Fourth step broken.';
   }
 
 
@@ -70,6 +55,5 @@
 
   // workingSteps
   console.log({ workingSteps: workingSteps() }); // Cambiar los valores de la línea 31 y esperar los resultados
-
 
 })();
